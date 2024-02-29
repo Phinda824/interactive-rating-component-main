2dcdd1c2b845b4bc8187007d2f47e16c18e47b68
Phinda <70489177+Phinda824@users.noreply.github.com>
2024-02-29 20:23:11 +0200

    adding functions

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-let container = document.querySelector('.section');
-let selectNo = document.querySelectorAll('.no_ratings .number');
-let submit = document.querySelector('button');
-let number = document.querySelector('span');
+let select_No = document.querySelectorAll('.no_ratings .number');
+let button = document.querySelector('button');
+let rate_no = document.querySelector('.rate');
+let card_section = document.querySelector('.section');
+let thank_card = document.querySelector('.thank-you');
 
-for(let i=0; i >= selectNo.length; i++){
-    selectNo[i].addEventListener('click', () =>{
-        rate.innerHtml = selectNo[i];
-    });
-    
-}
+let rate = null;
+
+select_No.forEach((ratings) =>{
+    ratings.addEventListener('click', (e) =>{
+        let checked_no = document.querySelector('.checked');
+        if(checked_no){
+            checked_no.classList.remove("checked");
+        }
+        let card = e.target;
+        card.classList.add("checked");
+        rate = e.target.innerText;
+    })
+})
+
+button.addEventListener('click', () => {
+    if(rate){
+        rate_no.innerText = rate;
+        card_section.classList.add("hidden");
+        thank_card.classList.remove("hidden");
+    }
+})
